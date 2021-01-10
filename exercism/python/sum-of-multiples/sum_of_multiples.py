cd3c007ee498de041f73d6086e2d1f56eac4f852
joswha <vladtoie@gmail.com>
2021-01-10 12:19:18 +0200

    added: solution for sum-of-multiples using list comprehension

---
+++ exercism/python/sum-of-multiples/sum_of_multiples.py
@@ -0,0 +1,4 @@
+def sum_of_multiples(limit, multiples):
+    x = [i for i in range(limit) for j in multiples if j!=0 and i % j == 0] # generate the multiples list
+    l = sorted(set(x), key=x.index) # sort the list, as there are multiple elements
+    return sum(l) # return the sum 
