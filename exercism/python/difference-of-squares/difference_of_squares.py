c31863c18a58462d9134df23209c3cbde96918cc
joswha <vladtoie@gmail.com>
2021-01-10 11:37:28 +0200

    added: difference-of-squares solution using list comprehension:

---
+++ exercism/python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,9 @@
+def square_of_sum(number):
+    return sum([i  for i in range(number + 1)]) ** 2
+
+def sum_of_squares(number):
+    return sum([i ** 2  for i in range(number + 1)])
+
+
+def difference_of_squares(number):
+    return square_of_sum(number) - sum_of_squares(number)
