54f546ce248edad4497796c926250e57258aef26
joswha <vladtoie@gmail.com>
2021-01-28 17:31:03 +0100

    updated: solution CH1.9, testcase was invalid

--- cracking-coding-interview/DataStructures/ArraysAndStrings/9.py
+++ cracking-coding-interview/DataStructures/ArraysAndStrings/9.py
@@ -1,5 +1,5 @@
 # Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to
-# isSubstring(e.g, "watterbottle" is rotation of "erbottlewat"
+# isSubstring(e.g, "waterbottle" is rotation of "erbottlewat"
 def isSubstring(s1, s2):
 
     if len(s1) != len(s2):
@@ -11,4 +11,4 @@ def isSubstring(s1, s2):
 
     return False
 
-print(isSubstring("hey", "yhe"))
+print(isSubstring("waterbottle", "erbottlewat"))
