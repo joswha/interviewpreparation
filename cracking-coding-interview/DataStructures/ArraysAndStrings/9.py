0dbc12a0e4c894b61631a2d4505df8d9004f0f07
joswha <vladtoie@gmail.com>
2021-01-28 17:27:56 +0100

    added: solution CH1.9; tricky using python's : delimiter on strings, might revise this for a proper solution

---
+++ cracking-coding-interview/DataStructures/ArraysAndStrings/9.py
@@ -0,0 +1,14 @@
+# Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to
+# isSubstring(e.g, "watterbottle" is rotation of "erbottlewat"
+def isSubstring(s1, s2):
+
+    if len(s1) != len(s2):
+        return False
+
+    for i in range(len(s1)):
+        if s1[i:] + s1[:i] == s2:
+            return True
+
+    return False
+
+print(isSubstring("hey", "yhe"))
