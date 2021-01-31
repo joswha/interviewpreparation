b93e92e1ffaa7854e834de2b313c2716e6d5aa4a
joswha <vladtoie@gmail.com>
2021-01-31 20:29:36 +0100

    updated: solution CH3.6, added challenge description

--- cracking-coding-interview/DataStructures/StacksAndQueues/6.py
+++ cracking-coding-interview/DataStructures/StacksAndQueues/6.py
@@ -1,3 +1,8 @@
+"""
+An animal shelter, which holds only dogs and cats, operates on a strictly "first in first out" basis. People must adopt either the oldest of all animals at the shelter
+or they can select whether they would prefer a dog or a cat(and will receive the oldest animal of that type). They cannot select which specific animal they would like. Create
+the data structure to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat. You may use the build-in LinkedList data structure.
+"""
 class Dog:
     def __init__(self):
         self.time = 0
