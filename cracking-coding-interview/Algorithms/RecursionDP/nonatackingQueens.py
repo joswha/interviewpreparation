128656c28fd2f02ac9d390e83e1738c965bfc7d0
joswha <vladtoie@gmail.com>
2021-03-04 12:19:38 +0200

    added: solution for python book on non-attacking queens

---
+++ cracking-coding-interview/Algorithms/RecursionDP/nonatackingQueens.py
@@ -0,0 +1,24 @@
+def n_queens(n):
+    def solve_n_queens(row):
+        if row == n:
+            # All queens are legally placed.
+            result.append(col_placement.copy())
+            return
+        for col in range(n):
+            # Test if a newly place queen will conflict any earlier queens place before
+            # Check the condition for the already existing queens on the table
+            # diagonal, vertical, orizontal lines
+            # c - col == 0 -> conflict in the columns
+            # c - col == i - row -> conflict diagonal up/ left
+            # c - col == row - i -> conflict diagonal up/ right
+            if all(abs(c - col) not in (0, row - i)
+                for i, c in enumerate(col_placement[:row])):
+                    col_placement[row] = col
+                    solve_n_queens(row + 1)
+
+    result = [[]]
+    col_placement = [0] * n
+    solve_n_queens(0)
+    return result
+
+print(n_queens(4))
